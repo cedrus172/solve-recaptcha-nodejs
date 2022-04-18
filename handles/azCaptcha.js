c8f8ece78f0a7a8315f655dcc67ff2f378765929
cedrus172 <cedrusisme@gmail.com>
2022-04-18 19:26:43 +0700

    last update

--- handles/azCaptcha.js
+++ handles/azCaptcha.js
@@ -1,26 +1,30 @@
-const axios = require('axios');
-const keyCaptcha = "wgkyhrdzbb4xjgfwrccjqptlf9638ypz";
+const axios = require("axios");
 
 function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    });
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
-exports.getIdCaptcha = async(url, googleKey) => {
-    let apiUrl = `https://azcaptcha.com/in.php?key=${keyCaptcha}&googlekey=${googleKey}&method=userrecaptcha&invisible=1&pageurl=${url}`;
-    let response = await axios.get(apiUrl);
-    return response.data.split('|')[1];
-}
+exports.getIdCaptcha = async (url, googleKey, keyCaptcha) => {
+  let apiUrl = `https://captcha69.com/in.php?key=${keyCaptcha}&googlekey=${googleKey}&method=userrecaptcha&invisible=1&pageurl=${url}`;
+  let response = await axios.get(apiUrl);
+  while (response.data.indexOf("ERROR_") > -1) {
+    await sleep(3000);
+    response = await axios.get(apiUrl);
+  }
+  return response.data.split("|")[1];
+};
 
-exports.getCaptchaCode = async(id) => {
-    let apiUrl = `https://azcaptcha.com/res.php?key=${keyCaptcha}&action=get&id=${id}`;
-    let response = await axios.get(apiUrl);
-    let count = 0;
-    while (response.data.indexOf('CAPCHA_NOT_READY') > -1) {
-        count++;
-        console.log(`Count ${count} - ` + response.data);
-        await sleep(5000);
-        response = await axios.get(apiUrl);
-    }
-    return response.data;
-}
+exports.getCaptchaCode = async (id, webName, keyCaptcha) => {
+  let apiUrl = `https://captcha69.com/res.php?key=${keyCaptcha}&action=get&id=${id}`;
+  let response = await axios.get(apiUrl);
+  let count = 0;
+  while (response.data.indexOf("CAPCHA_NOT_READY") > -1) {
+    count++;
+    if (count > 30) break;
+    console.log(`Count ${count} - ${webName} -  ` + response.data);
+    await sleep(5000);
+    response = await axios.get(apiUrl);
+  }
+  return response.data;
+};
--- main.js
+++ main.js
@@ -1,40 +1,149 @@
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+const PORT = 8000;
+var app = require("express")();
+var http = require("http");
+var sv = http.createServer(app);
+
+var express = require("express");
+
+app.use(express.json());
+app.use(cors(corsOptions)); // Use this after the variable declaration
+
+sv.listen(PORT, function () {
+  console.log("listening on *:" + PORT);
+});
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.get("/list", function (req, res) {
+  res.json(listCaptchaWeb);
+});
+
+app.get("/list/:webName", function (req, res) {
+  res.json(
+    listCaptchaWeb.filter((a) => a.webName == req.params.webName.toLowerCase())
+  );
+});
+
+app.get("/captcha/:webName", function (req, res) {
+  let findCaptcha = listCaptchaWeb.find(
+    (a) => a.webName == req.params.webName.toLowerCase()
+  );
+
+  listCaptchaWeb = listCaptchaWeb.filter((a) => a != findCaptcha);
+  res.json(findCaptcha);
+});
+
 let listCaptchaWeb = [];
-let listWeb = [{
-        webName: "pocinex-web",
-        googleKey: "",
-        url: "https://moonata1.net"
-    },
-    {
-        webName: "aresbo-web",
-        googleKey: "",
-        url: "https://ravo1.finance"
-    },
-    {
-        webName: "tlctrade-web",
-        googleKey: "6LcGyCAfAAAAABKPMWfnz1ZB8CK1b3LcYAKC-CSd",
-        url: "https://tlctrade1.net"
-    },
-    {
-        webName: "rosichi-web",
-        googleKey: "6LeaaYkcAAAAAEEbhDH0Pr5gFD74Z7BDMgQy2MbE",
-        url: "https://bitiva1net"
-    },
-    {
-        webName: "bodefi-web",
-        googleKey: "6LdTlxwbAAAAAEV8gvhUrLIT_TIP9X9-xgsJd-ug",
-        url: "https://bodefi1.net"
-    },
+let listWeb = [
+  {
+    webName: "pocinex-web",
+    googleKey: "",
+    url: "https://moonata1.net",
+    keyCaptcha: "thread_66a54241da3851e304520f092bfee687",
+    canSolve: false,
+  },
+  {
+    webName: "aresbo-web",
+    googleKey: "6LeEkukaAAAAALI6fsd1mVukHEuuVaRT4ckaXy-i",
+    url: "https://ravo1.finance",
+    keyCaptcha: "thread_66a54241da3851e304520f092bfee687",
+    canSolve: true,
+  },
+  {
+    webName: "tlctrade-web",
+    googleKey: "6LcGyCAfAAAAABKPMWfnz1ZB8CK1b3LcYAKC-CSd",
+    url: "https://tlctrade.net",
+    keyCaptcha: "thread_66a54241da3851e304520f092bfee687",
+    canSolve: false,
+  },
+  {
+    webName: "rosichi-web",
+    googleKey: "6LeaaYkcAAAAAEEbhDH0Pr5gFD74Z7BDMgQy2MbE",
+    url: "https://bitiva1.net",
+    keyCaptcha: "thread_66a54241da3851e304520f092bfee687",
+    canSolve: false,
+  },
+  {
+    webName: "bodefi-web",
+    googleKey: "6LdTlxwbAAAAAEV8gvhUrLIT_TIP9X9-xgsJd-ug",
+    url: "https://bodefi.net",
+    keyCaptcha: "thread_66a54241da3851e304520f092bfee687",
+    canSolve: false,
+  },
 ];
 const azCaptcha = require("./handles/azCaptcha");
 
-
 async function getCaptchaCode(webName) {
-    let webInfo = listWeb.find(a => a.webName == webName);
-    let id = await azCaptcha.getIdCaptcha(webInfo.url, webInfo.googleKey);
-    let captchaCode = await azCaptcha.getCaptchaCode(id);
-    console.log(captchaCode);
+  let webInfo = listWeb.find((a) => a.webName == webName);
+  let id = await azCaptcha.getIdCaptcha(
+    webInfo.url,
+    webInfo.googleKey,
+    webInfo.keyCaptcha
+  );
+  if (id != null) {
+    await sleep(5000);
+    let captchaCode = await azCaptcha.getCaptchaCode(
+      id,
+      webInfo.webName,
+      webInfo.keyCaptcha
+    );
+
+    if (captchaCode.indexOf("OK") > -1) {
+      addCaptchaToList(captchaCode.split("|")[1], webName, new Date());
+    }
+    console.log(webName, captchaCode, id);
+    getCaptchaCode(webName);
+  }
 }
 
-getCaptchaCode('bodefi-web');
-getCaptchaCode('rosichi-web');
-getCaptchaCode('tlctrade-web');
+function addCaptchaToList(captchaCode, webName, timeSolve) {
+  listCaptchaWeb.push({
+    captchaCode: captchaCode,
+    webName: webName,
+    timeSolve: timeSolve,
+  });
+}
+
+function startSolveCaptcha(thread) {
+  for (let i = 0; i < thread; i++) {
+    listWeb.forEach((web) => {
+      if (web.canSolve) {
+        getCaptchaCode(web.webName);
+      }
+    });
+  }
+
+  setInterval(filterCaptchaExpired, 500);
+}
+
+function filterCaptchaExpired() {
+  let tempCaptcha = [];
+
+  try {
+    listCaptchaWeb.forEach((captcha) => {
+      let newTime = new Date(captcha.timeSolve);
+      newTime.setSeconds(newTime.getSeconds() + 30);
+      if (newTime > new Date()) tempCaptcha.push(captcha);
+    });
+
+    listCaptchaWeb = tempCaptcha;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+startSolveCaptcha(10);
